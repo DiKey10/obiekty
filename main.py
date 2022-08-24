0986a9c02d7f936addc07978009b699257bd5a48
DiKey10 <kd930603@gmail.com>
2022-08-24 09:14:38 +0200

    das

---
+++ main.py
@@ -0,0 +1,17 @@
+class Car:
+   def __init__(self, make, model_name, top_speed, color):
+       self.make = make
+       self.model_name = model_name
+       self.top_speed = top_speed
+       self.color = color
+mustang = Car(make="Ford", model_name="Mustang", color="Yellow", top_speed=250)
+print(mustang)
+
+def __str__(self):
+    return f'{self.color} {self.make} {self.model_name}'
+print(__str__)
+print(mustang)
+
+
+
+
